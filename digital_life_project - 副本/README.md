fe675037da5b73f88efc51a2d65809ab2719bc29
deerestFarther <82210994+deerestFarther@users.noreply.github.com>
2023-11-27 11:38:35 +0800

    Delete digital_life_project - 副本 directory

--- digital_life_project - 副本/README.md
+++
@@ -1,2 +0,0 @@
-diffbir依赖triton安装问题：https://github.com/XPixelGroup/DiffBIR/issues/24
-diffbir依赖：https://github.com/XPixelGroup/DiffBIR
--- digital_life_project - 副本/config_diy.py
+++
@@ -1,9 +0,0 @@
-# 音频识别模型路径及大小
-WHISPER_PATH = "C:\\Users\\Admin\\Documents\\resources\\projects\\BatProjectsWithSourceCode\\vits-finetune\\VITS_fast_finetune\\whisper_model"
-WHISPER_MODEL_SIZE = "large"
-# 训练数据（未处理）路径
-DATASET_PATH = "C:\\Users\\Admin\\Documents\\resources\\projects\\PythonProjects\\digital_life_project\\raw_audio"
-# 此次训练数据（未处理）的名称（可自定义）
-DATASET_NAME = "wenjiang" # default
-# 使用模式（是否是推理模式）
-IS_INFERENCE = True
