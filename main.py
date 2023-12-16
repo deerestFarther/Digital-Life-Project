9d87b1e960197797f081d0c7e08820e8b8a3befb
fred-win10 <fred_bernard_chuck@protonmail.ch>
2023-12-16 12:25:41 +0800

    Adding new Feature vtalk

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import numpy as np
 from insightface.app import FaceAnalysis
 from sound.sound import SoundSeparator, SoundInference
 from face.swap_faces import FaceSwapper
+from face.vtalk import VideoRetalker
 from sound.tts import CoquiTTS
 # from face.diffbir import ImageProcessor
 
 # Define the list of supported languages (you may need to adjust this list based on the model's capabilities)
-supported_languages = ["en", "zh", "de", "fr", ...]  # Add other languages as supported by the model
+supported_languages = ["en", "zh", "de", "fr"]  # Add other languages as supported by the model
 
 
 MARKDOWN = \
@@ -64,25 +65,48 @@ with block:
         #     ]
         #     run_button.click(fn=image_processor.process, inputs=inputs, outputs=[result_gallery])
 
-        with gr.Tab(label="Inswapfaces"):
+        # with gr.Tab(label="Inswapfaces"):
+        with gr.Tab(label="Face-Swapping"):
             with gr.Row():
                 face_swapper = FaceSwapper()
 
                 with gr.Column():
-                    source_img = gr.Image(label="Original Photo", source="upload", type="pil")
-                    target_img = gr.Image(label="Target Photo", source="upload", type="pil")
-                    run_button = gr.Button(label="Run Face Swap")
+                    # source_img = gr.Image(label="Original Photo", source="upload", type="pil") # Gradio 3.x版本需要有source参数
+                    # target_img = gr.Image(label="Target Photo", source="upload", type="pil")
+                    source_img = gr.Image(label="Original Photo", type="pil")
+                    target_img = gr.Image(label="Target Photo", type="pil")
+                    # run_button = gr.Button(label="Run Face Swap") # Gradio 3.x版本需要label参数
+                    run_button = gr.Button("Run Face Swap") # Gradio 4.7.x版本不需要label参数
                     with gr.Accordion("Options", open=True):
                         swap_face_no = gr.Number(label="Swap Face Number", value=1)
                 with gr.Column():
-                    result_image = gr.Image(label="Result Photo", source="canvas", type="pil")
+                    # result_image = gr.Image(label="Result Photo", source="canvas", type="pil") # Gradio 3.x版本需要有source参数
+                    result_image = gr.Image(label="Result Photo", type="pil")
             inputs = [
                 source_img,
                 target_img,
                 swap_face_no
             ]
             run_button.click(fn=face_swapper.swap_faces, inputs=inputs, outputs=[result_image])
+
+        with gr.Tab(label="Video-Retalking"):
+            with gr.Row():
+                video_retalker = VideoRetalker()
+                with gr.Column():
+                    with gr.Row():
+                        seg = gr.Number(label="Segment length (Second), 0 for no segmentation")
+                    with gr.Row():
+                        v = gr.Video(label="Source Face")
+                        a = gr.Audio(type='filepath', label="Target Audio")
+                    with gr.Row():
+                        btn = gr.Button("Synthesize", variant="primary")
+                        
+                with gr.Column():
+                    output_video = gr.Video(label="Output Video")
     
+            btn.click(fn=video_retalker.convert, inputs=[seg, v, a], outputs=[output_video])
+
+
     with gr.Tab(label="CloneSounds"):
         sound_separator = SoundSeparator()
 
@@ -94,19 +118,22 @@ with block:
                 with gr.Column():
                     input_text = gr.Textbox(label="Enter text")
                     available_models = coqui_tts.get_available_models()
-                    model_dropdown = gr.Dropdown(label="Select TTS Model", choices=available_models, default=available_models[0] if available_models else None)
+                    # model_dropdown = gr.Dropdown(label="Select TTS Model", choices=available_models, default=available_models[0] if available_models else None)
+                    # Gradio 4.7.x版本不再像3.x版本那样使用default参数指定默认值，而是用value来指定
+                    model_dropdown = gr.Dropdown(label="Select TTS Model", choices=available_models, value=available_models[0] if available_models else None)
                     language_dropdown = gr.Dropdown(label="Select Language", choices=supported_languages)
-                    run_button = gr.Button(label="Convert to Speech")
+                    run_button = gr.Button("Convert to Speech")
                 with gr.Column():
                     result_audio = gr.Audio(label="Generated Speech")
             
             run_button.click(fn=coqui_tts.perform_tts, inputs=[input_text, model_dropdown, language_dropdown], outputs=result_audio)
             
-        with gr.Tab(label="Seperating"):
+        with gr.Tab(label="Music-Seperating"):
             with gr.Row():
                 with gr.Column():
-                    source_audio = gr.Audio(label="Original Audio", source="upload")
-                    run_button = gr.Button(label="Run Sound Separating")
+                    # source_audio = gr.Audio(label="Original Audio", source="upload") # Gradio 3.x版本需要有source参数
+                    source_audio = gr.Audio(label="Original Audio")
+                    run_button = gr.Button("Run Sound Separating")
                     with gr.Accordion("Options", open=True):
                         denoised = gr.Checkbox(label="Denoised?", value=True)
                 with gr.Column():
